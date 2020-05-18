d5ebf63b3d26392a70c520b888b59961419e4cc2
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-05-18 15:31:27 -0300

    Add files via upload

---
+++ operadoreslogicos2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class operadoreslogicos2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		Scanner leitor=new Scanner (System.in);
+		int idade;
+	
+		System.out.print("digite sua idade:");
+		idade  =leitor.nextInt();
+		
+		if(idade>=5 && idade<=10) 
+	System.out.println("categoria infantil");
+	
+	
+		if (idade>=11 && idade<=17)
+		System.out.println("categoria juvenil");
+		
+		if (idade>=18)
+	    System.out.println("categoria adulto");
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
